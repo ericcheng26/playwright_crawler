302f268cfd97c67f7a213115c2ecf27ed357902a
ericcheng26 <ericcheng26@gmail.com>
2021-04-09 18:21:43 +0800

    add

--- 儲存模組/savefile.py
+++
@@ -1,8 +0,0 @@
-def soup_tohtml():
-  body = (await page.content()).encode("utf8")
-  soup = BeautifulSoup(body, "lxml")
-  with open("output1.html", "w", encoding='utf-8') as file:
-    file.write(str(soup))
-
-def html_tojson():
-  
