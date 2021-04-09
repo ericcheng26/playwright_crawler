a2fa96548610f78eded50704d89bc0884c535722
ericcheng26 <ericcheng26@gmail.com>
2021-04-10 01:33:55 +0800

    typo

--- playwright_crawler/parser_package/yamol_parser/htmlToPdf.py
+++ playwright_crawler/parser_package/yamol_parser/htmlToPdf.py
@@ -6,20 +6,20 @@ import glob
 
 async def main():
     _pw = await async_playwright().start()
-    _browser = await _pw.chromium.launch(headless=False)
+    _browser = await _pw.chromium.launch(headless=True)
     _context = await _browser.new_context()
     page = await _context.new_page()
-    folder_path = '/some/path/to/file'
+    folder_path = '/home/eric/文件/html_soup'
     for filename in glob.glob(os.path.join(folder_path, '*.html')):
         with open(os.path.join(os.getcwd(), filename), 'r') as f:
             await page.setContent(f)
-            await page.pdf({
-                path: 'test.pdf',
-                format: 'A4',
-                margin: {
-                    top: "20px",
-                    left: "20px",
-                    right: "20px",
-                    bottom: "20px"
-                }
-            })
+            await page.pdf(
+                path=f'/home/eric/文件/pdf_soup/{filename}.pdf',
+                format='A4',
+            )
+# margin = {
+#     top: "20px",
+#     left: "20px",
+#     right: "20px",
+#     bottom: "20px"
+# }
