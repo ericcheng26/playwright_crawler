a126ca5a4e2e19d61a5a3e0df50746ced853df81
ericcheng26 <ericcheng26@gmail.com>
2021-04-11 21:25:34 +0800

    add

--- playwright_crawler/parser_package/yamol_parser/htmlToPdf.py
+++ playwright_crawler/parser_package/yamol_parser/htmlToPdf.py
@@ -10,29 +10,29 @@ pdf creation only work in chromium headless mode
     和Pdf需要存放的位置(pdfpath)
 '''
 parser = argparse.ArgumentParser(description='Process Html To Pdf.')
-parser.add_argument('-hp', dest='htmlpath',
-                    help='Fill the html directory')
-parser.add_argument('-pp', dest='pdfpath',
-                    help='Fill the pdf directory')
-args = parser.parse_args()
-htmlpath = args.htmlpath
-pdfpath = args.pdfpath
+parser.add_argument('--htmlpath', type=str, default='',
+                    help='The html directory')
+parser.add_argument('--pdfpath', type=str, default='',
+                    help='The pdf directory')
+parser.add_argument('--papersize', type=str, default='',
+                    help='paper format: A4,B5...')
 
 
-async def main(htmlpath, pdfpath):
+async def main():
+    args = parser.parse_args()
     _pw = await async_playwright().start()
     # pdf creation only work in chromium headless mode
     _browser = await _pw.chromium.launch(headless=True)
     _context = await _browser.new_context()
     page = await _context.new_page()
     # 對資料夾內的檔案，讀取和產生pdf
-    for filename in glob.glob(os.path.join(htmlpath, '*.html')):
-        with open(os.path.join(htmlpath, filename), 'r') as f:
+    for filename in glob.glob(os.path.join(args.htmlpath, '*.html')):
+        with open(os.path.join(args.htmlpath, filename), 'r') as f:
             await page.setContent(f)
             await page.emulate_media(media="screen", color_scheme="dark")
             await page.pdf(
-                path=f'{pdfpath}/{filename}.pdf',
-                format='A4'
+                path=f'{args.pdfpath}/{filename}.pdf',
+                format=f'{args.papersize}'
             )
 # margin = {
 #     top: "20px",
