deb6da9a9fa9f124bdb24401333a5e5fbe6e0db7
ericcheng26 <ericcheng26@gmail.com>
2021-04-16 21:56:43 +0800

    add

--- playwright_crawler/crawler_package/playwright_interaction/learningbar.py
+++ playwright_crawler/crawler_package/playwright_interaction/learningbar.py
@@ -3,6 +3,7 @@ from playwright.async_api import async_playwright
 
 
 async def main(playwright):
+    json_container = {}
     browser = await playwright.chromium.launch(headless=False)
     context = await browser.new_context(
         storage_state="/home/eric/文件/github/cookies_pack/learningbar_cookies")
@@ -39,21 +40,21 @@ async def main(playwright):
 
     # Click text=分類瀏覽
     await page.frame(name="LBR_Body").click("text=分類瀏覽")
-
-    # Select SUB-044
-    await page.frame(name="LBR_Body").select_option(
-        "select[name=\"s_Question_Subject\"]", "SUB-044")
-
-    # Click text=送出
-    await page.frame(name="LBR_Body").click("text=送出")
+    # 用一個迴圈將每次選擇後的行為包裝起來，因為每次選擇後做的行爲都一樣
+    # 每次選擇不同類別，進行相同行爲
+    for x in range(44, 98):
+        await page.frame(name="LBR_Body").select_option("select[name=\"s_Question_Subject\"]", f"SUB-0{x}")
+        await page.frame(name="LBR_Body").click("text=送出")
     # 找出含有"出處: 民國xxxxx" 的element並用list裝起來
-    lst_ = page.frame(name="LBR_Body").query_selector_all(
-        'text=/出處: 民國 \d{2,3} 年-寒?暑?期 獸醫[\u4e00-\u9fa5]*學 第 \d{1,2} 題/')
-    # 從上面的element list 取出element內部文字
-    for x in lst_:
-        str_ = x.text_content()
-    # Click img
-    await page.frame(name="LBR_Body").click("img")
+        lst_ = page.frame(name="LBR_Body").query_selector_all(
+            'text=/出處: 民國 \d{2,3} 年-寒?暑?期 獸醫[\u4e00-\u9fa5]*學 第 \d{1,2} 題/')
+        # 從element list 逐個取出element內部文字
+        # 並存進json
+        for x in lst_:
+            str_ = x.text_content()
+            json_container.join()
+        # 返回分類瀏覽
+        await page.frame(name="LBR_Body").click("img")
 
     # Select SUB-045
     await page.frame(name="LBR_Body").select_option(
