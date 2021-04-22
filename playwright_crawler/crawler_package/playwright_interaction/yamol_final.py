2fc668be11161d98eed9038c89666d7ae89f908b
ericcheng26 <ericcheng26@gmail.com>
2021-04-22 22:16:25 +0800

    fix interaction stablility

--- playwright_crawler/crawler_package/playwright_interaction/yamol_final.py
+++ playwright_crawler/crawler_package/playwright_interaction/yamol_final.py
@@ -15,33 +15,38 @@ o)"詳解卡解鎖"須要click觸發，儲存*_note.html
 
 
 async def main(page, _settingsdict):
-    time.sleep(2)
-
+    time.sleep(1)
     # right panel tab N="1-50"
     list_lv0element_handle = await page.query_selector_all(
         '#item_map_tab_0 a')
     for lv0element_handle in list_lv0element_handle:
-        await lv0element_handle.click(delay=1800)
-        time.sleep(1)
+        await lv0element_handle.click(delay=2000)
+
         # Press PageDown
         try:
-            await page.press("body", "PageDown", delay=15)
+            await page.press("body", "PageDown", delay=25)
+            time.sleep(1)
         except:
             await page.press(
-                "body:has-text(\"× 載入中..請稍候.. 關閉 通知 Mark as Read Templates Synced 20 Templates have been synced t\")", "PageDown", delay=15)
+                "body:has-text(\"× 載入中..請稍候.. 關閉 通知 Mark as Read Templates Synced 20 Templates have been synced t\")", "PageDown", delay=25)
+            time.sleep(1)
+
             # right panel tab N="51-100"
     await page.click('a[href="#item_map_tab_1"]')
     list_lv1element_handle = await page.query_selector_all(
         '#item_map_tab_1 a')
     for lv1element_handle in list_lv1element_handle:
-        await lv1element_handle.click(delay=1800)
-        time.sleep(1)
+        await lv1element_handle.click(delay=2000)
+
         # Press PageDown
         try:
-            await page.press("body", "PageDown", delay=15)
+            await page.press("body", "PageDown", delay=25)
+            time.sleep(1)
         except:
             await page.press(
-                "body:has-text(\"× 載入中..請稍候.. 關閉 通知 Mark as Read Templates Synced 20 Templates have been synced t\")", "PageDown", delay=15)
+                "body:has-text(\"× 載入中..請稍候.. 關閉 通知 Mark as Read Templates Synced 20 Templates have been synced t\")", "PageDown", delay=25)
+            time.sleep(1)
+    time.sleep(2)
     # prepare to save html's file name
     title = await page.title()
     str_subject_name = (await (await page.query_selector('text=/獸醫[\u4e00-\u9fa5]*學/')).text_content())[3:-4]
@@ -57,15 +62,17 @@ async def main(page, _settingsdict):
             html_file_name = f"{_settingsdict['SOUP_PATH']}/{match1.group(1)}_{match1.group(3) + '_' if match1.group(3) else ''}{str_subject_name}"
     else:
         html_file_name = f"{_settingsdict['SOUP_PATH']}/{title[-9:]}_{str_subject_name}"
-    time.sleep(2)
+
     # 點擊的按鈕可能已經被點擊過，所以沒有任何按鈕可以按，會TimeoutError
     try:
         list_lv3element_handle = await page.query_selector_all('text=/^查看全部\s*\d+\s*則討論$/')
         for lv3element_handle in list_lv3element_handle:
             await lv3element_handle.click(delay=3000)
 
+        time.sleep(3)
         list_lv2element_handle = await page.query_selector_all('text="詳解卡解鎖"')
         print(len(list_lv2element_handle))
+        time.sleep(1)
         for lv2element_handle in list_lv2element_handle:
             await lv2element_handle.click(delay=3000)
     except:
@@ -73,12 +80,12 @@ async def main(page, _settingsdict):
     # save html file, file name depend on match result
     with open(html_file_name + '.html', "w", encoding='utf-8') as file:
         file.write(str(BeautifulSoup((await page.content()).encode("utf8"), "lxml")))
-    time.sleep(2)
+    time.sleep(1)
     # 點擊私人筆記>>詳解卡解鎖>>save html
     list_lv4element_handle = await page.query_selector_all('text=/^私人筆記\(\s*\d+\s*\)$/')
     for lv4element_handle in list_lv4element_handle:
         await lv4element_handle.click(delay=3000)
-    time.sleep(2)
+    time.sleep(3)
     # 私人筆記中的詳解卡解鎖可能無東西可按
     try:
         list_lv5element_handle = await page.query_selector_all('text="詳解卡解鎖"')
