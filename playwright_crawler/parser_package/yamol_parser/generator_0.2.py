7f48fdbf469a798a378b36205349eb3ea7acc55a
ericcheng26 <ericcheng26@gmail.com>
2021-04-24 21:37:46 +0800

    add

--- playwright_crawler/parser_package/yamol_parser/generator_0.2.py
+++ playwright_crawler/parser_package/yamol_parser/generator_0.2.py
@@ -255,7 +255,7 @@ def generator(single_json_path):
         result += q_part_generator(q_list)
 
         d_tag_list = d_soup.select(
-            f'div[class^="{i}-"]') if d_soup != None else None
+            f'div[class^="{i}-"]') if n_soup != None else None
         n_tag_list = n_soup.select(
             f'div[class^="{i}-"]') if d_soup != None else None
 
